8a49c5be508df3792933b7e1417fefd016a78b15
erenming233@gmail.com <erenming233@gmail.com>
2021-04-21 19:11:19 +0800

    script for erda

--- build.sh
+++ build.sh
@@ -2,22 +2,17 @@
 
 set -o errexit -o nounset -o pipefail
 
-o="$(pwd)/filebeat-image"
-echo "${o}"
-
 v="$(head -n 1 VERSION)"
 v="${v}-$(date '+%Y%m%d')-$(git rev-parse --short HEAD)"
 
-image="registry.cn-hangzhou.aliyuncs.com/terminus/filebeat:${v}"
+image="${DOCKER_REGISTRY}/terminus/filebeat:${v}"
 
 docker build -t "${image}" \
     --label "branch=$(git rev-parse --abbrev-ref HEAD)" \
     --label "commit=$(git rev-parse HEAD)" \
     --label "build-time=$(date '+%Y-%m-%d %T%z')" \
     -f "Dockerfile" .
-docker login -u "${TERMINUS_DOCKERHUB_ALIYUN_USERNAME}" -p "${TERMINUS_DOCKERHUB_ALIYUN_PASSWORD}" registry.cn-hangzhou.aliyuncs.com
+docker login -u "${DOCKER_REGISTRY_USERNAME}" -p "${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
 docker push "${image}"
 
-cat > "${o}" <<EOF
-[{"module_name":"filebeat","image":"${image}"}]
-EOF
+echo "image=${image}" >> $METAFILE
