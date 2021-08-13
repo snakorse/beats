36b2d4c1bf240df7814004a32c9fc643518de0a4
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-08-13 22:28:52 +0800

    Use standard image naming (#22)
    * Use erda DOCKER_REGISTRY
    
    * note off

--- build.sh
+++ build.sh
@@ -5,14 +5,20 @@ set -o errexit -o nounset -o pipefail
 v="$(head -n 1 VERSION)"
 v="${v}-$(date '+%Y%m%d')-$(git rev-parse --short HEAD)"
 
-image="${DOCKER_REGISTRY}/terminus/filebeat:${v}"
+echo "version=${v}"
+
+image="${DOCKER_REGISTRY}/erda-filebeat:${v}"
+
+echo "image=${image}"
 
 docker build -t "${image}" \
     --label "branch=$(git rev-parse --abbrev-ref HEAD)" \
     --label "commit=$(git rev-parse HEAD)" \
     --label "build-time=$(date '+%Y-%m-%d %T%z')" \
     -f "Dockerfile" .
+
 docker login -u "${DOCKER_REGISTRY_USERNAME}" -p "${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
+
 docker push "${image}"
 
 echo "image=${image}" >> $METAFILE
